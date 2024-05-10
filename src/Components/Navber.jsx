67616c9847d68df1ea29a9d252a7b7bf3cb3192d
abunaim34 <farhanadnanfarabi@gmail.com>
2024-05-10 20:10:17 +0600

    this is sign in user

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -1,7 +1,9 @@
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../assets/logo-light.png'
+import useAuth from '../Hooks/useAuth';
 
 const Navber = () => {
+    const { user } = useAuth()
 
     const navLinks = <>
         <NavLink to="/" className={({ isActive }) => isActive ? 'border p-2 rounded-sm border-white text-white font-black' : 'font-bold text-white p-2'}>Home</NavLink>
@@ -27,33 +29,32 @@ const Navber = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                {/* <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                {
+                    user ? <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img title={user?.displayName} alt="Tailwind CSS Navbar component" src={user?.photoURL} />
+                            </div>
                         </div>
-                    </div>
-                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-black rounded-box w-52">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div> */}
-                <div className='flex gap-3'>
-                    <Link to='/register' className="relative hidden  md:inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-[#c59d5f] rounded hover:bg-[#c59d5f] group">
-                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#222222] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                        <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white font-bold">SignUp</span>
-                    </Link>
-                    <Link to='/login' className="relative  inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-[#c59d5f] rounded hover:bg-[#c59d5f] group">
-                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#222222] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                        <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white font-bold">SignIn</span>
-                    </Link>
-                </div>
+                        <ul tabIndex={0} className="mt-3 z-10 p-2 shadow menu menu-sm dropdown-content bg-black rounded-box w-52">
+                            
+                            <li className='hover:bg-gray-500 rounded-full'><Link to='/myAddedFood' >My added food items</Link></li>
+                            <li className='hover:bg-gray-500 rounded-full'><Link to='/addAFood'>Add a food item</Link></li>
+                            <li className='hover:bg-gray-500 rounded-full'><Link to='/MyOrderedFood'>My ordered food items</Link></li>
+                            <li className='hover:bg-gray-500 rounded-full'><a>Sign Out</a></li>
+                        </ul>
+                    </div> :
+                        <div className='flex gap-3'>
+                            <Link to='/register' className="relative hidden  md:inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-[#c59d5f] rounded hover:bg-[#c59d5f] group">
+                                <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#222222] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white font-bold">SignUp</span>
+                            </Link>
+                            <Link to='/login' className="relative  inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-[#c59d5f] rounded hover:bg-[#c59d5f] group">
+                                <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#222222] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white font-bold">SignIn</span>
+                            </Link>
+                        </div>
+                }
             </div>
         </div>
     );
